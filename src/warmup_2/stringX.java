758564c8310031fb4e3e7b6b667ea4b3db41695b
Haejin Yoon <hjyoomp@gmail.com>
2021-09-13 15:19:17 +0900

    210913 CodingBat commit

--- src/warmup_2/stringX.java
+++ src/warmup_2/stringX.java
@@ -3,11 +3,16 @@ package warmup_2;
 public class stringX {
 	public String stringX(String str) {
 		String result="";
+		if(str.length()==0)return "";
+		if(str.charAt(0)=='x')
+			result+="x";
 		for(int i=0;i<str.length();i++) {
 			if(str.charAt(i)!='x') {
 				result+=str.substring(i,i+1);
 			}
 		}
+		if(str.length()>1&&str.charAt(str.length()-1)=='x')
+			result+="x";
 		return result;
 	}
 }
--- src/warmup_2/stringYak.java
+++ src/warmup_2/stringYak.java
@@ -1,5 +1,25 @@
 package warmup_2;
 
 public class stringYak {
-
+	public static void main(String[] args) {
+		System.out.println(stringYak("xxxyakyyyakzzz"));
+		
+	}
+	
+	
+	public static String stringYak(String str) {
+		String result="";
+		if(str.length()<3)return str;
+		for(int i=0;i<str.length();i++) {
+			System.out.println("first str: "+str);
+			if(str.substring(i,i+3).equals("yak")) {
+				result+=str.substring(0,i);
+				str=str.substring(i+3,str.length());
+				System.out.println("str: "+str);
+				System.out.println("result :" + result);
+			}
+		}
+		result+=str;
+		return result;
+	}
 }
