e8f27483a0320c4abfd2899dc1245d9658869978
Haejin Yoon <hjyoomp@gmail.com>
2021-09-14 09:01:34 +0900

    210914 1st commit

--- src/warmup_2/stringYak.java
+++ src/warmup_2/stringYak.java
@@ -2,24 +2,69 @@ package warmup_2;
 
 public class stringYak {
 	public static void main(String[] args) {
+		System.out.println(stringYak("HiyakHi"));
 		System.out.println(stringYak("xxxyakyyyakzzz"));
+		System.out.println(stringYak("yakxxxyak"));
+		System.out.println(stringYak("yak123ya"));
 		
 	}
 	
 	
 	public static String stringYak(String str) {
 		String result="";
-		if(str.length()<3)return str;
 		for(int i=0;i<str.length();i++) {
-			System.out.println("first str: "+str);
-			if(str.substring(i,i+3).equals("yak")) {
-				result+=str.substring(0,i);
-				str=str.substring(i+3,str.length());
-				System.out.println("str: "+str);
-				System.out.println("result :" + result);
+			if(i+2<str.length() && str.charAt(i)=='y'&&str.charAt(i+2)=='k') {
+				i+=2;
+			}else {
+				result+=str.charAt(i);
 			}
 		}
-		result+=str;
 		return result;
 	}
 }
+
+
+//		String result="";
+//		int tmp=0;
+//		for(int i=0;i<str.length()-2;i++) {
+//			System.out.println(i);
+//			if(str.substring(i,i+3).equals("yak")) {
+//				i+=2;
+//				if(i==str.length()) {
+//					break;
+//				}
+//				System.out.println("2i: "+i);
+//			}else {
+//				result+=str.substring(i,i+1);
+//				System.out.println("1: "+result);
+//				if(i>=str.length()-3) {
+//					result+=str.substring(str.length()-2,str.length());
+//					System.out.println("2: "+result);
+//				}
+//			}
+//		}
+//		return result;
+
+//		String result="";
+//		int count=0;
+//		if(str.length()<3)return str;
+//		for(int i=0;i<str.length()-2;i++) {
+//			System.out.println(i);
+//			if(str.substring(i,i+3).equals("yak")) {
+//				if(count==0) {
+//					result+=str.substring(0,i);
+//					System.out.println("result :" + result);
+//					System.out.println("str: "+str);
+//				}
+//				
+//				if(count>0) { 
+//					result+=str.substring(i,i+3);
+//					System.out.println("result :" + result);
+//					
+//					System.out.println("str: "+str);
+//				}
+//				count++;
+//			}
+//		}
+//		result+=str;
+//		return result;
